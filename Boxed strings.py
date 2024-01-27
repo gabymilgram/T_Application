2314496d82a29d9dc0e02244832d2309e2de0342
gabymilgram <148977331+gabymilgram@users.noreply.github.com>
2024-01-26 21:13:43 -0500

    Add files via upload

---
+++ Boxed strings.py
@@ -0,0 +1,11 @@
+
+# function that takes a list of strings an prints them, one per line, in a rectangular frame.
+def boxed_strings(string_list):
+    longest = max(string_list, key = len) # determine longest word
+    print('*' * (len(longest)+4)) # print top line of border
+    for word in string_list:
+        print("* " + word + " " * (len(longest) - len(word)+1) + "*") # format words
+    print('*' * (len(longest)+4)) # print bottom line of border
+
+
+
